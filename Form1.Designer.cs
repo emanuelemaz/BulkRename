5bd54262042d4cd5b4da27b7f45bc05f9f98dfef
emanuelemaz <emanuelemaz@outlook.it>
2020-02-16 04:28:43 +0100

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,270 @@
+﻿namespace BulkRename
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.File = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modifica = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(305, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(392, 27);
+            this.button1.TabIndex = 1;
+            this.button1.TabStop = false;
+            this.button1.Text = "Seleziona i file";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.InitialDirectory = "C:\\Users\\%USER%\\Desktop";
+            this.openFileDialog1.Multiselect = true;
+            this.openFileDialog1.Title = "Seleziona i file da rinominare.";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(703, 69);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(287, 26);
+            this.textBox1.TabIndex = 4;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Aggiungi",
+            "Rimuovi",
+            "Modifica"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 71);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(287, 24);
+            this.comboBox1.TabIndex = 5;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(703, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(287, 26);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Aggiungi/Rimuovi";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(823, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Modifica";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(12, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(287, 26);
+            this.textBox3.TabIndex = 9;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(507, 69);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(190, 27);
+            this.button2.TabIndex = 10;
+            this.button2.TabStop = false;
+            this.button2.Text = "Esegui";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(305, 69);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(190, 27);
+            this.button3.TabIndex = 11;
+            this.button3.TabStop = false;
+            this.button3.Text = "Anteprima";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 110);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(978, 425);
+            this.dataGridView1.TabIndex = 12;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column1.HeaderText = "File";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            this.Column1.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column1.Width = 48;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column2.HeaderText = "Modifica";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            this.Column2.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column2.Width = 72;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.Column3.HeaderText = "Risultato";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            this.Column3.Resizable = System.Windows.Forms.DataGridViewTriState.False;
+            this.Column3.Width = 73;
+            // 
+            // File
+            // 
+            this.File.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.File.HeaderText = "File";
+            this.File.Name = "File";
+            // 
+            // Modifica
+            // 
+            this.Modifica.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
+            this.Modifica.HeaderText = "Modifica";
+            this.Modifica.Name = "Modifica";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(966, 11);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(24, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "?";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1002, 547);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Bulk Rename";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn File;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Modifica;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,313 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace BulkRename
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+        }
+
+        string fileNames { get; set; }
+        List<string> fileNamesList = new List<string>();
+        string a { get; set; }
+        List<string> fileNamesListnoext = new List<string>();
+        List<ListViewItem> filenomi = new List<ListViewItem>();
+        List<string> risultato = new List<string>();
+        string [] fileNamesArray2 { get; set; }
+        string[] fileNamesArraydef { get; set; }
+        public void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            fileNames = string.Join("\n", openFileDialog1.FileNames);
+            string[] fileNamesArray = openFileDialog1.FileNames;
+            fileNamesArray2 = fileNamesArray;
+            string a = textBox3.Text;
+            for (int i = 0; i < fileNamesArray.Length; i++)
+            {
+                fileNamesList.Add(Path.GetFileName(fileNamesArray[i]));
+            }
+
+            if (openFileDialog1.FileName == "openFileDialog1")
+            {
+                dataGridView1.Rows.Clear();
+                dataGridView1.Columns.Clear();
+                MessageBox.Show("Non hai inserito nessun file.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(1);
+            }
+
+            for (int i = 0; i < fileNamesArray.Length; i++)
+            {
+                string s;
+                s = System.IO.Path.GetFileNameWithoutExtension(fileNamesArray[i]);
+                fileNamesListnoext.Add(s);
+            }
+
+            for (int i = 0; i < fileNamesArray.Length; i++)
+            {
+                dataGridView1.Rows.Add(fileNamesListnoext[i]);
+            }
+
+            if (dataGridView1.Rows[0].Cells[0].Value != "") { button1.Enabled = false; }
+
+            dataGridView1.AutoResizeColumns();
+            dataGridView1.ClearSelection();
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem == comboBox1.Items[0])
+            {
+                textBox1.ReadOnly = true;
+                textBox1.Enabled = false;
+                textBox2.ReadOnly = true;
+                textBox2.Enabled = false;
+                textBox3.ReadOnly = false;
+                textBox3.Enabled = true;
+            }
+
+            if (comboBox1.SelectedItem == comboBox1.Items[1])
+            {
+                textBox1.ReadOnly = true;
+                textBox1.Enabled = false;
+                textBox2.ReadOnly = true;
+                textBox2.Enabled = false;
+                textBox3.ReadOnly = false;
+                textBox3.Enabled = true;
+            }
+            if (comboBox1.SelectedItem == comboBox1.Items[2])
+            {
+                textBox1.ReadOnly = false;
+                textBox1.Enabled = true;
+                textBox2.ReadOnly = false;
+                textBox2.Enabled = true;
+                textBox3.ReadOnly = true;
+                textBox3.Enabled = false;
+            }
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if ((Convert.ToString(dataGridView1[1, 0].Value) == ""))
+                {
+                    button3.PerformClick();
+                }
+            }
+            catch (System.ArgumentOutOfRangeException) { 
+                MessageBox.Show("Non hai inserito nessun file.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(1);
+            }
+            int j = 0;
+            string[] fileNamesTmp = fileNamesArray2;
+            fileNamesArraydef = fileNamesArray2;
+            List<string> fileNamesListTmp = new List<string>();
+            try { fileNamesListTmp = fileNamesTmp.ToList(); }
+            catch (System.ArgumentNullException) { 
+                MessageBox.Show("Non hai inserito nessun file.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(1);
+            }
+            foreach (string s1 in fileNamesArraydef)
+            {
+                int ind = fileNamesArray2[j].LastIndexOf("\\");
+                int ind2 = fileNamesArray2[j].LastIndexOf(".");
+                int ind3 = fileNamesArray2[j].Length;
+
+                try
+                {
+                    fileNamesArraydef[j] = fileNamesArray2[j].Substring(0, ind) + "\\" + risultato[j] + fileNamesArray2[j].Substring(ind2, fileNamesArray2[j].Length - ind2);
+                }
+
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show("Non hai inserito alcuna modifica.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    System.Environment.Exit(1);
+                }
+                    j++;
+            }
+
+            DialogResult dres = MessageBox.Show("Se ci sono file che otterranno lo stesso nome, non verrano rinominati tutti e l'operazione non andrà a buon fine. Assicurati che tutti i file abbiano nomi diversi dopo la modifica, altrimenti l'operazione non andrà a buon fine.\n\nL'operazione è irreversibile.\n\nVuoi continuare?", "Attenzione!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+            if (dres == DialogResult.Yes)
+            {
+                j = 0;
+                foreach (string s2 in fileNamesArraydef)
+                {
+                    try
+                    {
+                        System.IO.File.Move(@fileNamesListTmp[j], @fileNamesArraydef[j]);
+                        j++;
+                    }
+                    catch (System.IO.IOException)
+                    {
+                        MessageBox.Show("Esiste almeno un file con lo stesso nome del risultato, l'operazione non può essere eseguita.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        System.Environment.Exit(1);
+                    }
+                }
+            }
+            if (dres == DialogResult.No)
+            {
+                MessageBox.Show("Hai deciso di uscire dal programma.", "Uscita dal programma", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                try 
+                {
+                    GC.Collect();
+                    System.Environment.Exit(1); 
+                }
+                catch (System.ComponentModel.Win32Exception) { System.Environment.Exit(1); }
+            }
+
+
+            MessageBox.Show("File rinominati correttamente.");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        public void button3_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            dataGridView1.Columns.Clear();
+
+            dataGridView1.Columns.Add("File", "File");
+            dataGridView1.Columns.Add("Modifica", "Modifica");
+            dataGridView1.Columns.Add("Risultato", "Risultato");
+
+            risultato.Clear();
+            a = textBox3.Text;
+
+            try
+            {
+                for (int i = 0; i < fileNamesArray2.Length; i++)
+                {
+
+                    if (comboBox1.SelectedItem == comboBox1.Items[0])
+                    {
+
+                        risultato.Add(Convert.ToString(fileNamesListnoext[i]) + a);
+
+                    }
+                    if (comboBox1.SelectedItem == comboBox1.Items[1])
+                    {
+                        try { risultato.Add(Convert.ToString(fileNamesListnoext[i].Remove(fileNamesListnoext[i].Length - Convert.ToInt32(textBox3.Text)))); }
+                        catch (System.ArgumentOutOfRangeException)
+                        {
+                            MessageBox.Show("Stai cercando di rimuovere più caratteri di quelli esistenti.\n\n\t\t\tUscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            GC.Collect();
+                            System.Environment.Exit(1);
+                        }
+                        a = "- " + Convert.ToInt32(textBox3.Text);
+
+                    }
+                    if (comboBox1.SelectedItem == comboBox1.Items[2])
+                    {
+                        a = textBox1.Text;
+                        try { risultato.Add(Convert.ToString(fileNamesListnoext[i].Remove(fileNamesListnoext[i].Length - Convert.ToInt32(textBox2.Text))) + a); }
+                        catch (System.FormatException)
+                        {
+                            MessageBox.Show("Non hai inserito alcuna modifica.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            System.Environment.Exit(1);
+                        }
+
+                        catch (System.ArgumentOutOfRangeException)
+                        {
+                            MessageBox.Show("Stai cercando di rimuovere più caratteri di quelli esistenti.\n\n\t\t\tUscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            GC.Collect();
+                            System.Environment.Exit(1);
+                        }
+                    }
+                }
+            }
+
+            catch (System.NullReferenceException) {
+                MessageBox.Show("Non hai inserito nessun file.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                System.Environment.Exit(1);
+            }
+
+            for (int i = 0; i < fileNamesArray2.Length; i++)
+            {
+                try
+                {
+                    dataGridView1.Rows.Add(fileNamesListnoext[i], a, risultato[i]);
+                }
+
+                catch (System.ArgumentOutOfRangeException)
+                {
+                    MessageBox.Show("Non hai inserito alcuna modifica.\n\n\t\t Uscita dal programma.", "Errore", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    System.Environment.Exit(1);
+                }
+            }
+
+            dataGridView1.AutoResizeColumns();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void objectListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Aggiungi: aggiungi una stringa di testo ai nomi.\nRimuovi: rimuovi un certo numero di caratteri dalla fine dei nomi.\nModifica: rimuovi un certo numero di caratteri e inseriscine altri per sostituirli.\n\nSviluppato da: Emanuele Mazzucchi.\n\nCC BY-SA\nQuest'opera è stata rilasciata con licenza Creative Commons Attribuzione - Condividi allo stesso modo 4.0 Internazionale. Per leggere una copia della licenza visita il sito web http://creativecommons.org/licenses/by-sa/4.0/.", "Informazioni", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
